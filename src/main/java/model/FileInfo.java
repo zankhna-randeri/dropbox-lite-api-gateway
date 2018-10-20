0eb6828b995b1a586ea2cc640cd3cd49bfe6fa90
Zankhna Randeri <zankhna.randeri@gmail.com>
2018-10-20 02:52:23 -0700

    Model class represents file details stored in DB

---
+++ src/main/java/model/FileInfo.java
@@ -0,0 +1,25 @@
+package model;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * FileInfo contains
+ *  - fileId : unique id of file
+ *  - fileName : name of file
+ *  - userId : id of file owner
+ *  - fileCreationTimestamp : file creation timestamp
+ *  - fileUpdateTimestamp : file last modification timestamp
+ *  - s3Key : s3Key representing file location in s3 bucket.
+ */
+
+@Data
+@Builder
+public class FileInfo {
+  private String fileId;
+  private String fileName;
+  private int userId;
+  private long fileCreationTimestamp;
+  private long fileUpdateTimestamp;
+  private String s3Key;
+}
