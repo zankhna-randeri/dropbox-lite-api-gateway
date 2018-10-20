35083196e933e59086df3bc71a4d3c05907e883a
Zankhna Randeri <zankhna.randeri@gmail.com>
2018-10-20 02:57:56 -0700

    Added code to handle json request in api.

---
+++ src/main/java/activity/RegisterController.java
@@ -0,0 +1,34 @@
+package activity;
+
+import model.RegisterOutput;
+import model.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import service.S3service;
+import service.UserService;
+
+@RestController
+public class RegisterController {
+
+  @PostMapping(value = "/user", consumes = {"application/json"})
+  public RegisterOutput createUser(@RequestBody User user) {
+
+    //1. check user already registered
+    //2. create folder in s3
+    //3. insert user in rds mysql
+
+    UserService userService = new UserService();
+    if (userService.isUserExist()) {
+      return RegisterOutput.builder().userCreated(false).build();
+    } else {
+
+      S3service s3Service = new S3service();
+      s3Service.createFolder(user.getUserEmail());
+
+      return RegisterOutput.builder().userCreated(true).build();
+    }
+
+
+  }
+}
---
+++ src/main/java/model/RegisterOutput.java
@@ -0,0 +1,12 @@
+package model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Builder
+@ToString
+public class RegisterOutput {
+  private boolean userCreated;
+}
