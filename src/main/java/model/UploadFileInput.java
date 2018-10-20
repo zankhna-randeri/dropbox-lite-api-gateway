f87e01dbc1496d0f821f294c6a007ac9a5a6db52
Zankhna Randeri <zankhna.randeri@gmail.com>
2018-10-20 02:59:22 -0700

    Minor changes

---
+++ src/main/java/model/UploadFileInput.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@ToString
+public class UploadFileInput {
+  private MultipartFile fileInput;
+  private String folderName;
+}
